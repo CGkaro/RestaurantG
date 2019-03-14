34febcc20fe75041d9b16d49d2973badf54ce445
Carlos <practicantes4@lopez-fernandez.com>
2019-03-13 17:48:16 -0700

    establish contact with contentful

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,10 @@
 import React from "react"
 //import { Link } from "gatsby"
-import { FaBeer } from "react-icons/fa"
+
 import Layout from "../components/layout"
 //import Image from "../components/image"
 import SEO from "../components/seo"
-import styled from "styled-components"
+
 import { PageHeader, Banner } from "../utils"
 import ctImg from "../images/bcg/contactBcg.jpeg"
 
@@ -18,8 +18,4 @@ const ContactPage = () => (
   </Layout>
 )
 
-const ButtonWrapper = styled.button`
-  background: var(--mainOrange);
-  color: white;
-`
 export default ContactPage
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,10 +1,9 @@
 import React from "react"
 //import { Link } from "gatsby"
-import { FaBeer } from "react-icons/fa"
+
 import Layout from "../components/layout"
 //import Image from "../components/image"
 import SEO from "../components/seo"
-import styled from "styled-components"
 import { PageHeader, Banner } from "../utils"
 import mnImg from "../images/bcg/menuBcg.jpeg"
 
@@ -18,8 +17,4 @@ const MenuPage = () => (
   </Layout>
 )
 
-const ButtonWrapper = styled.button`
-  background: var(--mainOrange);
-  color: white;
-`
 export default MenuPage
